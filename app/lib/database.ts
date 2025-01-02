a6969297762d3f8c1b65cbb14258bc20a14950a1
winifredogbeiwi <imadeogbeiwi@gmail.com>
2025-01-03 00:53:45 +0100

    deployment fixes

--- app/lib/database.ts
+++ app/lib/database.ts
@@ -1,6 +1,7 @@
 
 import { db } from "@/firebase";
 import { collection, getDocs } from "firebase/firestore";
+import { QuestionsType } from "../utils/type";
 
 export const fetchCategories = async () => {
   const categoriesSnapshot = await getDocs(collection(db, "categories"));
@@ -12,9 +13,22 @@ export const fetchQuizzes = async (categoryId: string) => {
   return quizzesSnapshot.docs.map(doc => ({ id: doc.id, ...doc.data() }));
 };
 
-export const fetchQuestions = async (categoryId: string, quizId: string) => {
+export const fetchQuestions = async (
+  categoryId: string,
+  quizId: string
+): Promise<QuestionsType[]> => {
   const questionsSnapshot = await getDocs(
     collection(db, `categories/${categoryId}/quizzes/${quizId}/questions`)
   );
-  return questionsSnapshot.docs.map(doc => ({ id: doc.id, ...doc.data() }));
+
+  return questionsSnapshot.docs.map((doc) => {
+    const data = doc.data();
+    return {
+      id: doc.id,
+      questionText: data.questionText || "Default question text",
+      option: data.option || ["Option 1", "Option 2", "Option 3", "Option 4"],
+      answer: data.answer || "Option 1",
+      explanation: data.explanation || "Default explanation",
+    } as QuestionsType;
+  });
 };
